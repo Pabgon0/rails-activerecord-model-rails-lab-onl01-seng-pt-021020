cb180e274e080e8085a4b8a6ea425954e92ade8a
Pablo Gonzalez <pabgon0@gmail.com>
2020-06-09 01:51:28 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,6 @@
 class Student < ActiveRecord::Base
   def student_summary
-    name = self.first_name + " " + self.last_name
-    name.to_s
+    name = self.first_name.to_s + " " + self.last_name.to_s
     name
   end
 end
